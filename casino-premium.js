8a991c6b8c925c85c83868b09ca270cc7fa64bb7
Farid Store <faridjafar04@gmail.com>
2026-08-02 11:58:29 +0700

    Add UI elements for balance and spin total

--- casino-premium.js
+++ casino-premium.js
@@ -4,6 +4,7 @@
 ========================================================== */
 
 const UI = {
+   
 
 balance:document.getElementById("displayBalance"),
 spinTotal:document.getElementById("spinTotal"),
